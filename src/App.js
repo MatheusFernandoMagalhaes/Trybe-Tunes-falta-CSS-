66880c99500dbdcc6d9dae396d5b51dcd23c5f8f
MatheusFernandoMagalhaes <matheusf.magalhaes@hotmail.com>
2022-07-01 15:52:30 -0300

    Requisito 1 feito

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
 
-class App extends React.Component {
+export default class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
-    // Iniciando projeto
+    return (
+      <BrowserRouter>
+        <p>TrybeTunes</p>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route path="/search" component={ Search } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="/profile" component={ Profile } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
-
-export default App;
