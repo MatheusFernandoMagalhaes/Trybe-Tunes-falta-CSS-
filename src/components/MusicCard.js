9dd0df4a487d7ed335c4c83ee52093d550b19bcb
MatheusFernandoMagalhaes <matheusf.magalhaes@hotmail.com>
2022-07-09 18:57:17 -0300

    Requisito 7 feito

---
+++ src/components/MusicCard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default class MusicCard extends React.Component {
+  render() {
+    const { music } = this.props;
+    return (
+      <div>
+        <p>{ music.trackName }</p>
+        <audio data-testid="audio-component" src={ music.previewUrl } controls>
+          <track kind="captions" />
+          O seu navegador não suporta o elemento
+          {' '}
+          {' '}
+          <code>audio</code>
+          .
+        </audio>
+      </div>
+    );
+  }
+}
--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,13 +1,57 @@
 import React from 'react';
-import Header from '../components/Header';
+import PropTypes from 'prop-types';
+// import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
+import Loading from '../components/Loading';
+import MusicCard from '../components/MusicCard';
+import '../style/Album.css';
 
 export default class Album extends React.Component {
+  state = ({
+    resultsApi: [],
+    isLoading: true,
+  })
+
+  componentDidMount() {
+    this.getSongs();
+  }
+
+  getSongs = async () => {
+    const { match: { params: { id } } } = this.props;
+    const musics = await getMusics(id);
+    console.log(musics);
+    this.setState({
+      resultsApi: musics,
+      isLoading: false,
+    });
+  };
+
   render() {
+    const { resultsApi, isLoading } = this.state;
     return (
-      <div data-testid="page-album">
-        <Header />
-        <p>Album</p>
+      <div data-testid="page-album" className='musics-content'>
+        { isLoading ? <Loading /> : (
+          <>
+            <div>
+              <p data-testid="artist-name">{resultsApi[0].artistName}</p>
+              <p data-testid="album-name">
+                {resultsApi[0].collectionName}
+              </p>
+            </div>
+            <div>
+              {resultsApi.map((music, index) => (
+                index !== 0 && (
+                  <MusicCard key={ music.trackId } music={ music } />
+                )
+              ))}
+            </div>
+          </>
+        )}
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.string.isRequired,
+};
