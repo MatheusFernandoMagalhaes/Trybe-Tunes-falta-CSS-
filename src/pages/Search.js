9f89ba2991cfa1bb8b0b94c5f8db5705c7f349f4
Matheus Fernando <matheusf.magalhaes@hotmail.com>
2023-05-11 18:14:15 -0300

    Adicionando estilização para a página de search

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Loading from '../components/Loading';
 import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import '../style/Search.css';
 
 export default class Search extends React.Component {
   state = {
@@ -46,9 +47,8 @@ export default class Search extends React.Component {
   render() {
     const { disabled, musicOrArtist, isLoading, resultsApi, showResults } = this.state;
     return (
-      <div data-testid="page-search">
+      <div data-testid="page-search" id="search-container">
         <Header />
-        <p>Search</p>
         { isLoading ? <Loading /> : (
           <>
             <input
@@ -70,27 +70,34 @@ export default class Search extends React.Component {
           </>
         )}
         { showResults && resultsApi.length !== 0 && (
-          <div>
-            <p>
+          <>
+            <h1>
               Resultado de álbuns de:
               {' '}
               { showResults }
-            </p>
-            { resultsApi.map((artist) => (
-              <Link
-                to={ `/album/${artist.collectionId}` }
-                key={ artist.collectionId }
-                data-testid={ `link-to-album-${artist.collectionId}` }
-              >
-                <img src={ artist.artworkUrl100 } alt={ artist.artistName } />
-                <p>
-                  { artist.artistName }
-                </p>
-                <p>
-                  { artist.collectionName }
-                </p>
-              </Link>))}
-          </div>
+            </h1>
+            <div id="music-card-container">
+              { resultsApi.map((artist) => (
+                <div
+                  id="music-card-div"
+                  key={ artist.collectionId }
+                >
+                  <Link
+                    to={ `/album/${artist.collectionId}` }
+                    data-testid={ `link-to-album-${artist.collectionId}` }
+                  >
+                    <img src={ artist.artworkUrl100 } alt={ artist.artistName } />
+                    <p>
+                      { artist.artistName }
+                    </p>
+                    <p>
+                      { artist.collectionName }
+                    </p>
+                  </Link>
+
+                </div>))}
+            </div>
+          </>
         )}
         { showResults && resultsApi.length === 0 && (
           <p>Nenhum álbum foi encontrado</p>
